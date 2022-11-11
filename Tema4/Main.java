66e973f040a6fa7296cfcbced41b8a9da69dec15
LopezJuanJ <juanjavierlopezgarrido@gmail.com>
2022-11-11 17:22:50 +0100

    Upload T4

---
+++ Tema4/Main.java
@@ -0,0 +1,9 @@
+package Tema4;
+
+public class Main {
+    public static void main(String[] args) {
+        Numeros numeros = new Numeros();
+        numeros.valores = new int[10];
+        System.out.println(numeros.valores.length);
+    }
+}
